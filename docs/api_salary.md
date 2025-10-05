63024a41b53e74c9ab481b13c900f30850a93ce1
ESCINF <jaurelio.sanchez.salazar@gmail.com>
2025-10-04 18:21:28 -0600

    Docs edited and docker support

---
+++ docs/api_salary.md
@@ -0,0 +1,4 @@
+# API Reference (salary)
+
+::: src.salary.salary
+
--- src/salary.py
+++ src/salary.py
@@ -3,11 +3,10 @@
 def salary(hours, hourly_wage):
     """Calculates the salary considering the number of hours and the hourly wage.
     
-    Exaple:
-        >>> salary(30,2000)
+    Examples:
+        >>> salary(30,2000) 
+        60000
         
-        (60000)
-
     Args:
         hours(int): number of hours
         hourly_wage(int): hourly wage
@@ -20,8 +19,16 @@ def salary(hours, hourly_wage):
     else:
         return 40*hourly_wage+(hours-40)*hourly_wage*1.5
 
+
 def main():
-    print(salary(30,2000))
+    """Main function that invokes salary function.
+    """
+    try:
+        hours=int(input("Ingrese el numero de horas: "))
+        wage=int(input("Ingrese el monto por hora: "))
+        print(salary(hours,wage))
+    except:
+            print("Dato incorrecto!!")    
 
 if __name__ == "__main__":
     main()
