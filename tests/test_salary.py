bafdd50d35c14664726d4a710083005a81cf3073
ESCINF <jaurelio.sanchez.salazar@gmail.com>
2025-10-15 20:04:31 -0600

    Prueba con error en test

--- tests/test_salary.py
+++ tests/test_salary.py
@@ -5,7 +5,7 @@ from src.salary import salary
 class TestSalary(unittest.TestCase):
 
     def test_salario(self):
-        self.assertEqual(salary(30, 2000), 60000)
+        self.assertEqual(salary(30, 2000), 50000)
         self.assertEqual(salary(50, 2000), 110000)
 
 if __name__ == '__main__':
